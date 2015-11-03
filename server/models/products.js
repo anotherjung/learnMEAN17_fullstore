63ea1ad7521f47f56e9ef0487b0257fea48837e0
Jung No <jungno@gmail.com>
2015-11-02 18:50:04 -0800

    added default hosted responsive img

--- server/models/products.js
+++ server/models/products.js
@@ -6,7 +6,7 @@ var Schema = mongoose.Schema;
 //start model 
 var ProductSchema = new mongoose.Schema({
 	name: String,
-	imageurl: String,
+	imageurl: { type: String, default: 'http://app.resrc.it/s=w1280,pd2/o=85/http://www.your-site.co/image.jpg'},
 	qty: String,
 	created: {type: Date, default: new Date}
 });
